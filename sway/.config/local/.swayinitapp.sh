ef9fe9d11727848144278e255a22c4ff2b191350
brett beloin <brettbeloin@gmail.com>
2026-03-27 04:19:35 -0600

    fixes

--- sway/.config/local/.swayinitapp.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if ! pgrep ghostty >/dev/null; then echo "Launching ghostty..." && swaymsg "exec /usr/bin/ghostty"; fi
-if ! pgrep alacritty >/dev/null; then echo "Launching alacritty..." && swaymsg "exec /usr/bin/alacritty"; fi
-if ! pgrep chrome >/dev/null; then echo "Launching chrome..." && swaymsg "exec flatpak run com.google.Chrome"; fi
-if ! pgrep steam >/dev/null; then echo "Launching steam..." && swaymsg "exec /usr/bin/steam"; fi
-# if ! pgrep Discord >/dev/null; then echo "Launching discord..." && swaymsg "exec flatpak run com.discordapp.Discord"; fi
