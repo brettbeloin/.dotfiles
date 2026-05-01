33a9f0f4a9b1d14c544abbd0728743899466c617
brett beloin <brettbeloin@gmail.com>
2026-05-01 17:56:17 -0600

    add error messages to screen

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -7,6 +7,10 @@ vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.opt.swapfile = false
 vim.opt.termguicolors = true
-vim.opt.shell = "/usr/bin/zsh"
+vim.opt.shell = "/usr/bin/bash"
+vim.diagnostic.config({
+    virtual_text = true, -- Set to false to hide inline messages
+    signs = true,
+})
 
 vim.cmd.colorscheme("everforest")
