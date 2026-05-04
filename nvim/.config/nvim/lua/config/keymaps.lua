75dc60601ae6a2ed1357dbdef080ea42a65233ac
brett beloin <brettbeloin@gmail.com>
2026-05-04 08:06:44 -0600

    allow easier split terms

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,4 +5,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Exit to file tree :Ex" }
 vim.keymap.set("n", "<leader>so", vim.cmd.so, { desc = "Source current file :so" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
+vim.keymap.set('n', '<leader>sv', ':vsplit | terminal<CR>', { desc = "open virtical terminal" })
+vim.keymap.set('n', '<leader>sh', ':split | terminal<CR>', { desc = "open horizantal terminal" })
 -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "format documant" })
