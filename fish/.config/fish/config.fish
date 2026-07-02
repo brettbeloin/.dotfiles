1530cc283d545178b2386fc1eac11dbf02a52835
brett beloin <brettbeloin@gmail.com>
2026-07-02 02:21:38 -0600

    edit fish to detect os

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,16 +1,46 @@
 # Sway autostart on TTY1
 
-if status is-login
-    if test (tty) = /dev/tty1
-        if test -f ~/.config/fish/conf.d/laptop.conf
-            exec dbus-run-session sway --unsupported-gpu
-        else
-            exec dbus-run-session sway
-        end
-    end
+# if status is-login
+#     if test (tty) = /dev/tty1
+#         if test -f ~/.config/fish/conf.d/laptop.conf
+#             exec dbus-run-session sway --unsupported-gpu
+#         else
+#             exec dbus-run-session sway
+#         end
+#     end
+# end
+
+# Extract the ID line from os-release and strip out the "ID=" text
+set -l os_id (string match -r '^ID=(.+)' < /etc/os-release)[2]
+
+# Strip any surrounding quotes if they exist
+set os_id (string trim -c '"' $os_id)
+
+# Execute behavior based on the specific distribution
+if test "$os_id" = "cachyos"
+    echo "You are running CachyOS (Arch-based)"
+    # Add your pacman/paru aliases or configuration here
+    source /usr/share/wikiman/widgets/widget.fish
+
+    set -gx XCURSOR_SIZE 24
+
+    alias poweroff="sudo loginctl poweroff"
+    alias reboot="sudo loginctl reboot"
+    alias pkgSearch="pacman -Qi"
+    alias search="pacman -Ss"
+
+else if test "$os_id" = "fedora"
+    echo "You are running Fedora (RPM-based)"
+    # Add your dnf aliases or configuration here
+    alias poweroff="sudo systemctl poweroff"
+    alias reboot="sudo systemctl reboot"
+    alias pkgSearch="dnf info"
+    alias search="dnf search --all"
+
+else
+    echo "You are running an unsupported OS: $os_id"
 end
 
-source /usr/share/wikiman/widgets/widget.fish
 
 # if test -f ~/.setup.sh
 #     bash .setup.sh
@@ -23,9 +53,6 @@ source /usr/share/wikiman/widgets/widget.fish
 # add stuff to the path
 set fish_greeting
 set -gx MANPAGER 'nvim +Man!'
-#set -gx XCURSOR_THEME WinSur-dark-cursors
-set -gx XCURSOR_SIZE 24
-#set -gx XDG_SCREENSHOTS_DIR ~/Pictures/Screenshots/
 set -gx NODE_PATH $HOME/.local/lib/node_modules $NODE_PATH
 set -gx npm_config_prefix $HOME/.local
 set -gx PATH $PATH $HOME/go/bin
@@ -46,8 +73,6 @@ fish_add_path /home/linuxbrew/.linuxbrew/bin/lua-language-server
 # my custom aliases
 alias sysEdit='nvim ~/.config/fish/config.fish && source ~/.config/fish/config.fish'
 alias sysRead='cat ~/.config/fish/config.fish'
-alias poweroff="sudo loginctl poweroff"
-alias reboot="sudo loginctl reboot"
 alias vim='nvim'
 alias gEmacs='emacsclient -c -a "nvim"'
 alias tEmacs='emacsclient -t -a "nvim"'
@@ -57,8 +82,6 @@ alias ls="ls -lh --color=auto"
 alias la="ls -lAh --color=auto"
 alias rm="rm -r"
 alias mkdir="mkdir -pv"
-alias pkgSearch="xbps-query -Rs"
-alias search="xbps-query -S"
 alias cd="z"
 alias ..="cd .."
 alias 2b="cd ../.."
