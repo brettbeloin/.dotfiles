1611965f54ff913456f03fd2163a28321abd0389
brett beloin <brettbeloin@gmail.com>
2026-07-10 21:25:04 -0600

    create and commed out function for oil

--- nvim/.config/nvim/lua/config/editor/options.lua
+++ nvim/.config/nvim/lua/config/editor/options.lua
@@ -12,6 +12,13 @@ vim.opt.shell = "/usr/bin/bash"
 vim.opt.spell = true
 vim.opt.spelllang = { "en_us" }
 
-
+-- vim.api.nvim_create_autocmd("VimEnter", {
+--     callback = function()
+--         if vim.fn.argc() == 0 then
+--             vim.cmd("Oil")
+--         end
+--     end,
+-- })
+--
 -- Colorscheme
 vim.cmd.colorscheme("everforest")
